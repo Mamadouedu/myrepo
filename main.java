5fde575bf1adc68001a0153aaa559e7be209f000
Mamadouedu <mamadou.niangadou.edu@gmail.com>
2025-02-11 16:49:30 +0100

    mise à jour now

--- main.java
+++ main.java
@@ -1,3 +1,21 @@
+class Voiture {
+    String couleur;
+    String marque;
+    String modele;
+
+    void demarrer() {
+        System.out.println("La voiture " + marque + " " + modele + " démarre.");
+    }
+
+    void accelerer() {
+        System.out.println("La voiture " + marque + " " + modele + " accélère.");
+    }
+
+    void freiner() {
+        System.out.println("La voiture " + marque + " " + modele + " freine.");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Voiture voiture = new Voiture();
